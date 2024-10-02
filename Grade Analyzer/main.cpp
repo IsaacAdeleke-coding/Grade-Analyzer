6a4f2a21eb39be00e549f5591482ab18dd385ff0
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-10-02 10:29:46 -0700

    Initial Commit

---
+++ Grade Analyzer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Grade Analyzer
+//
+//  Created by Isaac Adeleke on 10/2/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
